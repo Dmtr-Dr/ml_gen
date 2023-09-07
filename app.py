3a421e574a3c0d39db5c5b6d0d5bd52741b40af0
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-07 23:09:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Molecules")
 molecule = st.text_input('Input Molecule', 'CH-Co')
@@ -9,3 +10,12 @@ def predict():
     st.success("It's done")
 
 st.button('Predict',on_click=predict)
+
+
+chart_data = np.random.randn(20, 3)
+
+# Создаем график
+plt.plot(chart_data)
+
+# Отображаем график на странице
+st.pyplot(plt)
