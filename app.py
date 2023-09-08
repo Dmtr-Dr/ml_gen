cd57af545f9b20b4edec96a3a8b0bbf84c9ff721
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-08 19:32:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Draw
-from PIL import Image
+import matplotlib.pyplot as plt
 
 st.title("Molecules")
 molecule = st.text_input('Input Molecule', 'O=C(O)Cc1ccc(SSc2ccc(CC(=O)O)cc2)cc1')
@@ -15,8 +15,8 @@ st.button('Predict',on_click=predict)
 
 core = Chem.MolFromSmiles(molecule)
 img = Draw.MolToImage(core)
-pil_img = Image.fromarray(img)
-st.image(pil_img, caption='Молекула', use_column_width=True)
+img_matplotlib = Draw.MolToMPL(img)
+st.pyplot(img_matplotlib, clear_figure=True)
 
 
 uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
