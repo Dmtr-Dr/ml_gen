22604f8ad9f87070f5478f48655097ab9ac9e465
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-08 20:54:05 +0300

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,1 +1,9 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+import matplotlib.pyplot as plt
 
+def mol_image(chemical_formula):
+  molecule = Chem.MolFromSmiles(chemical_formula)
+  img = Draw.MolToImage(molecule)
+  img_matplotlib = Draw.MolToMPL(molecule)
+  return(img_matplotlib)
