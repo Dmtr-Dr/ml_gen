068654d2700b31199c3fb4e08574c26db085c131
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-08 21:42:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,11 @@ def predict():
 
 st.button('Predict',on_click=predict)
 
-if molecule is not None:
-    mol = Chem.MolFromSmiles(molecule)
-    img = Draw.MolToImage(mol)
-    img_matplotlib = Draw.MolToMPL(mol)
-    st.pyplot(plt, clear_figure=True)
-else:
-    st.error("Некорректная SMILES-нотация молекулы")
+mol = Chem.MolFromSmiles(molecule)
+img = Draw.MolToImage(mol)
+img_matplotlib = Draw.MolToMPL(mol)
+st.pyplot(plt, clear_figure=True)
+
 
 uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
 
