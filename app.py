d16301e8178bd1e67c53247e19502b1e706f49f2
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-09 01:02:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ from utils import *
 st.title("Tool для определения молекул-ингибиторов и блокираторов репликации ВИЧ")
 molecule = st.text_input('Input Molecule', 'CCO')
 
-st.button('Predict',on_click=predict)
-
 mol = Chem.MolFromSmiles(molecule)
 if mol is None:
     st.write("Некорректная SMILES-нотация молекулы")
@@ -24,22 +22,23 @@ else:
     img_matplotlib = Draw.MolToMPL(mol)
     st.pyplot(plt, clear_figure=True)
 
-featurizer = dc.feat.CircularFingerprint(size=1024)  # Фичи
-ecfp = featurizer.featurize(list(molecule))
+def predict():
+    featurizer = dc.feat.CircularFingerprint(size=1024)  # Фичи
+    ecfp = featurizer.featurize(list(molecule))
+    
+    smiles_dataset = dc.data.NumpyDataset(X=ecfp)  # Датасет
+    
+    model = dc.models.MultitaskClassifier(
+        1,
+        1024,
+        layer_sizes=[1000],
+        dropouts=[.25],
+        learning_rate=0.001,
+        batch_size=50)  # batch_size default 50
+    
+    set_checkpoint(model, 'hiv')  # Устанавливаем чекпоинты
+    
+    out = model.predict(smiles_dataset)
+    st.write(out)
 
-smiles_dataset = dc.data.NumpyDataset(X=ecfp)  # Датасет
-
-model = dc.models.MultitaskClassifier(
-    1,
-    1024,
-    layer_sizes=[1000],
-    dropouts=[.25],
-    learning_rate=0.001,
-    batch_size=50)  # batch_size default 50
-
-set_checkpoint(model, 'hiv')  # Устанавливаем чекпоинты
-
-out = model.predict(smiles_dataset)
-st.write(out)
-
-
+st.button('Predict',on_click=predict)
