da2ca9b7c0c48ab2813ee4455d35b9c2af182a0c
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-08 20:10:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from rdkit import Chem
-from rdkit.Chem import Draw
-import matplotlib.pyplot as plt
-from pillow import Image
+from openbabel import pybel
 
 
 st.title("Molecules")
@@ -15,10 +12,20 @@ def predict():
 
 st.button('Predict',on_click=predict)
 
-core = Chem.MolFromSmiles(molecule)
-img = Draw.MolToImage(core)
-pil_img = Image.fromarray(img)
-st.image(pil_img, caption='Молекула', use_column_width=True)
+def draw_molecule(molecule_text):
+    try:
+        # Преобразование текста молекулы в изображение
+        mol = pybel.readstring("smi", molecule_text)
+        mol.make3D()
+        mol.draw(format="png", filename="molecule.png", overwrite=True)
+
+        # Отображение изображения в Streamlit
+        st.image("molecule.png", use_column_width=True)
+    except Exception as e:
+        st.error(f"Произошла ошибка при отображении молекулы: {e}")
+
+if molecule:
+    draw_molecule(molecule)
 
 uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
 
