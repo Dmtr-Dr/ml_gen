bcedd55b9f891698c52f5515eee85eb1bc6178a4
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-08 21:03:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from extra import mol_image
-
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from PIL import Image
+import io
 
 st.title("Molecules")
 molecule = st.text_input('Input Molecule', 'O=C(O)Cc1ccc(SSc2ccc(CC(=O)O)cc2)cc1')
@@ -12,9 +14,12 @@ def predict():
 
 st.button('Predict',on_click=predict)
 
-
-if molecule:
-    mol_image(molecule)
+mol = Chem.MolFromSmiles(molecule)
+img = AllChem.MolToImage(mol, size=(300, 300))
+img_byte_array = io.BytesIO()
+img.save(img_byte_array, format="PNG")
+img_data = img_byte_array.getvalue()
+st.image(Image.open(io.BytesIO(img_data)), caption="Молекула", use_column_width=True)
 
 uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
 
