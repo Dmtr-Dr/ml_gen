056b21928c6408a92aee21c429428c365dfce70f
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-09 00:01:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,34 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 import matplotlib.pyplot as plt
 
-st.title("Tool для определения молекул-ингибиторов и блокираторов репликации ВИЧ")
-molecule = st.text_input('Input Molecule', 'CCO')
+def page_1():
+    st.title("Tool для определения молекул-ингибиторов и блокираторов репликации ВИЧ")
+    molecule = st.text_input('Input Molecule', 'CCO')
 
-def predict():
-    st.success("It's done")
+    def predict():
+        st.success("It's done")
 
-st.button('Predict',on_click=predict)
+    st.button('Predict',on_click=predict)
 
-mol = Chem.MolFromSmiles(molecule)
-if mol is None:
-    st.write("Некорректная SMILES-нотация молекулы")
+    mol = Chem.MolFromSmiles(molecule)
+    if mol is None:
+        st.write("Некорректная SMILES-нотация молекулы")
+    else:
+        img = Draw.MolToImage(mol)
+        img_matplotlib = Draw.MolToMPL(mol)
+        st.pyplot(plt, clear_figure=True)
+
+
+    uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
+
+    if uploaded_file is not None:
+        file_contents = uploaded_file.read()
+        st.write("Содержимое загруженного файла:")
+        st.write(file_contents)
+def page_2():
+    st.title("Модель 2")
+
+if page == "Tool для определения молекул-ингибиторов и блокираторов репликации ВИЧ":
+    page_1()
 else:
-    img = Draw.MolToImage(mol)
-    img_matplotlib = Draw.MolToMPL(mol)
-    st.pyplot(plt, clear_figure=True)
-
-
-uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
-
-if uploaded_file is not None:
-    # Считываем содержимое загруженного файла
-    file_contents = uploaded_file.read()
-
-    # Отображаем содержимое файла
-    st.write("Содержимое загруженного файла:")
-    st.write(file_contents)
+    page_2()
