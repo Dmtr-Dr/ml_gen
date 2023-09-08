3674ef21067a610678dafa6dc12536a3916e7ba6
Dmitry Druchinin <90934478+Dmtr-Dr@users.noreply.github.com>
2023-09-08 21:08:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Draw
-from PIL import Image
-import io
+import matplotlib.pyplot as plt
 
 st.title("Molecules")
 molecule = st.text_input('Input Molecule', 'CC0')
@@ -16,7 +15,12 @@ st.button('Predict',on_click=predict)
 
 mol = Chem.MolFromSmiles(molecule)
 img = Draw.MolToImage(mol, size=(300, 300))
-st.image(Image.fromarray(img), caption="Молекула", use_column_width=True)
+img_array = Draw.MolToImage(mol, size=(300, 300), kekulize=True)
+img_data = plt.imread(img_array)
+plt.figure(figsize=(6, 6))
+plt.imshow(img_data)
+plt.axis('off')
+st.pyplot()
 
 uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
 
